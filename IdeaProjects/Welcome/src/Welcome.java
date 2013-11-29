49be237c90d4af30d07e3f70f4a2f2cebd2d0e2d
cagri <cagri.cakir@iaau.edu.kg>
2013-11-29 19:45:19 +0600

    Bu benim IntelliJ IDEA platformumda ki ilk projem. Sadece test içeriklidir.

---
+++ IdeaProjects/Welcome/src/Welcome.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 29.11.2013
+ * Time: 18:03
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class Welcome
+{
+    public static void main(String[]arg)
+    {
+        System.out.println("Hello IntelliJ!..");
+    }
+}
